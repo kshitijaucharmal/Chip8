feb3810fef90232e3e083adcf10d4d3230eb309f
Kshitij Aucharmal <kshitijaucharmal21@gmail.com>
2025-09-30 21:02:32 +0530

    raylib added

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
-#include <iostream>
+#include <raylib.h>
 
 int main() {
-    std::cout << "Hello World" << std::endl;
+    int screenwidth = 64;
+    int screenheight = 32;
+
+    int tilesize = 8;
+
+    InitWindow(screenwidth * tilesize, screenheight * tilesize, "Chip8");
+
+    while (!WindowShouldClose()) {
+
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        DrawText("Ready", 100, 100, 18, BLACK);
+
+        EndDrawing();
+    }
+    CloseWindow();
     return 0;
 }
